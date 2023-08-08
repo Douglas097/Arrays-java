3074c83d20b8e1fbb286ff588e9c206cc0e5e012
Douglas097 <douglascarvalhodasilva123@gmail.com>
2023-08-08 10:12:33 -0300

    Array

--- java/Array18.java
+++ java/Array18.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Array18 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int MAX_DAYS = 10;
+        double[] precos = new double[MAX_DAYS];
+        
+        for (int i = 0; i < MAX_DAYS; i++) {
+            System.out.print("Digite o preço da ação no dia " + (i + 1) + ": ");
+            precos[i] = scanner.nextDouble();
+        }
+        
+        double maiorVariacao = 0.0;
+        
+        for (int i = 0; i < MAX_DAYS - 1; i++) {
+            double variacao = Math.abs(precos[i + 1] - precos[i]);
+            if (variacao > maiorVariacao) {
+                maiorVariacao = variacao;
+            }
+        }
+                         
+        System.out.println("A maior variação de preço entre dois dias consecutivos é: " + maiorVariacao);
+        
+        scanner.close();
+    }
 }
